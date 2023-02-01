b809a239068ac7132f0594f2dd13995779e514a5
DanielML98 <60166480+DanielML98@users.noreply.github.com>
2023-02-01 12:43:17 -0600

    Add files via upload

---
+++ Kanye Quote Generator/Kanye Quote Generator/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  Kanye Quote Generator
+//
+//  Created by Daniel Martínez on 12/02/22.
+//
+
+import Foundation
+
+typealias KanyeQuoteResult = (Result<String, Error>) -> Void
+
+class NetworkManager {
+  class func fetchQuote(completion: @escaping KanyeQuoteResult) {
+    let url: URL? = URL(string: "https://api.kanye.rest/text")
+    URLSession.shared.dataTask(with: url!) { data, response, error in
+      if let error = error {
+        print("There was an error \(error)")
+        return
+      }
+      guard let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) else {
+              return
+            }
+      if let data = data,
+        let amazingQuote = String(data: data, encoding: .utf8) {
+        print(amazingQuote)
+        completion(.success(amazingQuote))
+      } else {
+        print ("This, actually went wrong")
+      }
+    }.resume()
+  }
+}
---
+++ Kanye Quote Generator/Kanye Quote Generator/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Kanye Quote Generator
+//
+//  Created by Daniel Martínez on 12/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  let quoteLabel: UILabel = {
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 200))
+    label.numberOfLines = 0
+    label.textColor = .black
+    return label
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.addSubview(quoteLabel)
+    quoteLabel.center = self.view.center
+  }
+  
+  override func viewDidLayoutSubviews() {
+    NetworkManager.fetchQuote { [unowned self] result in
+      switch result {
+      case .success(let quote):
+        DispatchQueue.main.async {
+          self.quoteLabel.text = quote
+        }
+      case .failure(_):
+        print("Oh, oh, something went wrong")
+      }
+    }
+  }
+}
+
+
